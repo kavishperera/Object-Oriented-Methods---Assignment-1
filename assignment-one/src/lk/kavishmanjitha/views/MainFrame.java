9f8099d00e8f0de3c25d8ae36779e910d89de0ed
KavishManjithaPerera <IAmking789456>
2021-01-14 15:59:50 +0530

    create ui - skeleton

--- assignment-one/src/lk/kavishmanjitha/views/MainFrame.java
+++ assignment-one/src/lk/kavishmanjitha/views/MainFrame.java
@@ -3,15 +3,41 @@ package lk.kavishmanjitha.views;
 import lk.kavishmanjitha.Constant;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
     public MainFrame() {
-        pack();
         setSize(Constant.FRAME_WIDTH, Constant.FRAME_HEIGHT);
         setTitle(Constant.APPLICATION_TITLE);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setBackground(Color.WHITE);
+        gamePanel.setPreferredSize(new Dimension(500, 500));
+        gamePanel.setBounds(0, 0, 500, 500);
+        add(gamePanel, BorderLayout.NORTH);
+
+        JButton startButton = new JButton();
+        startButton.setText("START");
+        startButton.setBackground(Color.GREEN);
+        add(startButton, BorderLayout.WEST);
+
+        JLabel statusLable = new JLabel();
+        statusLable.setText("SCORE : 0");
+        statusLable.setBorder(new EmptyBorder(0, 50, 0, 0));
+        add(statusLable, BorderLayout.CENTER);
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                System.out.println("Start");
+            }
+        });
     }
 }
